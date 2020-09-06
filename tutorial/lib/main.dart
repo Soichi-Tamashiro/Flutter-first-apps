c99966fcce35ada0b5b9a70b45215a52f312e797
Soichi Tamashiro <wyvernys@live.com>
2020-09-06 18:21:00 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   tutorial/android/app/build.gradle
    	modified:   tutorial/lib/main.dart
    	modified:   tutorial/pubspec.yaml

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,8 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:image_picker_gallery_camera/image_picker_gallery_camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -50,30 +55,66 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
+  int _currentIndex = 0;
+  File _image;
+  String basename;
+  // Future<Directory> _externalDocumentsDirectory;
 
-  void _onItemTapped(int index) {
+  Future getImage(ImgSource source) async {
+    var image = await ImagePickerGC.pickImage(
+        context: context,
+        source: source,
+        cameraIcon: Icon(
+          Icons.add,
+          color: Colors.red,
+        ),
+        cameraText: Text(
+          "From Camera",
+          style: TextStyle(color: Colors.red),
+        ) //cameraIcon and galleryIcon can change. If no icon provided default icon will be present
+        );
     setState(() {
-      _selectedIndex = index;
+      _image = image;
+      String basename = _image.path;
+      String basename_last = _image.path.split("/")?.last;
+      // print(basename);
+      // replaceAll(basename, basename_last);
+      String newDirect = basename.replaceFirst(basename_last, "");
+      // print(newDirect);
+      var now = new DateTime.now();
+      var formatter = new DateFormat('yyyy-MM-dd_HH-mm-ss');
+      String formattedDate = formatter.format(now);
+      // print(formattedDate); // 2020-09-06_18-16-01
+      String mPath = newDirect + '${formattedDate}_receta.jpg';
+      print(mPath);
+      // getExternalStorageDirectory();
+      // String imgType = path.split('.').last;
+      // String mPath =
+      //     '${directory.path.toString()}/${DateTime.now()}_receta.$imgType';
+      // String mPath =
+      //     '${getExternalStorageDirectory()}/Pictures/${DateTime.now()}_receta.jpg';
+      // var myFile = new File(
+      //     '${getExternalStorageDirectory()}/${DateTime.now()}_receta.jpg');
+      // myFile = image;
+      _image = _image.renameSync(mPath);
+      // _image = image;
     });
   }
 
+  final tabs = [
+    Center(
+      child: Text('Galleria'),
+    ),
+    Center(
+      child: Text(
+        'Para Agregar Nuevas Recetas Volver a Presionar el Botón de Nueva Receta',
+        textAlign: TextAlign.center,
+      ),
+    ),
+    Center(
+      child: Text('busqueda'),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -89,28 +130,48 @@ class _BottomNavBarState extends State<BottomNavBar> {
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body: tabs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            title: Text('Nueva Receta'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo_library),
-            title: Text('Galería'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Buscar Receta'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo_library),
+              title: Text('Galería'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_a_photo),
+              title: Text('Nueva Receta'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Buscar Receta'),
+            ),
+          ],
+          selectedItemColor: Colors.amber[800],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+              if (_currentIndex == 1) {
+                getImage(ImgSource.Camera);
+              }
+            });
+          }),
     );
+
+    // Future<File> _saveImageToDisk(File myfile, Directory directory) async {
+    //   setState(() {
+    //     try {
+    //       //getImage(ImgSource.Both);
+    //       File tempFile = myfile;
+    //       // File image = tempFile.readAsBytesSync();
+    //       String imgType = path.split('.').last;
+    //       String mPath =
+    //           '${directory.path.toString()}/${DateTime.now()}_receta.$imgType';
+    //       tempFile = tempFile.renameSync(mPath);
+    //     } catch (e) {
+    //       //
+    //     }
+    //   });
+    // }
   }
 }
